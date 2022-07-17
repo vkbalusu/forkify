5854e5cec9b9f7a3213cdb46405878eab2d47f81
Vamsi Krishna Balusu <vkbalusu@gmail.com>
2022-07-16 20:00:48 -0700

    render bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 const spinner = document.querySelector('.spinner');
@@ -48,6 +49,8 @@ const controlAddBookMark = function () {
 
   // 2) Update recipe view
   recipeView.render(model.state.recipe);
+
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,29 @@
+import View from './view';
+import icons from 'url:../../img/icons.svg';
+class BookMarksView extends View {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = 'No bookmarks yet ;)';
+  _message = '';
+
+  _generateMarkup() {
+    return this._data
+      .map(result => this._generatePreviewMarkup(result))
+      .join('');
+  }
+
+  _generatePreviewMarkup(result) {
+    return `<li class="preview">
+              <a class="preview__link" href="#${result.id}">
+                <figure class="preview__fig">
+                  <img src="${result.image}" alt="Test" />
+                </figure>
+                <div class="preview__data">
+                  <h4 class="preview__title">${result.title}</h4>
+                  <p class="preview__publisher">${result.publisher}Woman</p>
+                </div>
+              </a>
+            </li>`;
+  }
+}
+
+export default new BookMarksView();
